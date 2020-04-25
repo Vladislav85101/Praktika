bbf89c3244c12796c106935791caa64b739b02bb
Vladislav85101 <64206393+Vladislav85101@users.noreply.github.com>
2020-04-25 14:44:39 +0300

    Programm

---
+++ cso/Examin/Mass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Examin
+{
+    class Mass
+    {
+        public string Name;
+        public string Owner;
+        public int Page;
+
+        public Mass(string Name, string Owner, int Page)
+        {
+            this.Name = Name;
+            this.Owner = Owner;
+            this.Page = Page;
+        }
+
+    }
+}
---
+++ cso/Examin/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Examin
+{
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                int MassCount = 0;
+                Console.Write("Введите размер массива:");
+                MassCount = Convert.ToInt32(Console.ReadLine());
+                Mass[] newspaper = new Mass[MassCount];
+                for (int i = 0; i <= MassCount; i++)
+                {
+                    Console.WriteLine("Введите количество страниц:");
+                    newspaper[i].Page = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Введите название:");
+                    newspaper[i].Name = Console.ReadLine();
+                    Console.WriteLine("Введите владельца:");
+                    newspaper[i].Owner = Console.ReadLine();
+                }
+
+            }
+            public static void SaveFile(Mass[] newspaper)
+            {
+                using (StreamWriter pass = new StreamWriter("Newspaper.txt"))
+                {
+                    foreach (var i in newspaper)
+                    {
+                    pass.WriteLine("Название" + i.Name + "Владелец" + i.Owner + "Количество страниц" + i.Page + "\n");
+                    }
+                }
+            }
+        }
+        
+}   
